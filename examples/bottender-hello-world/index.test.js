cc7b76ea234b4f1b58da9df46cbdfe0944409a99
hch903 <houtsen@gmail.com>
2020-12-15 16:30:14 +0800

    examples: example revised

--- examples/bottender-hello-world/index.test.js
+++ examples/bottender-hello-world/index.test.js
@@ -1,3 +1,4 @@
+const bottender = require('bottender');
 const testing = require('../../dist/index');
 
 const PSID = '0000000000000000';
@@ -8,6 +9,8 @@ const lineUser = new testing.LineUser();
 
 const mInterceptor = new testing.MessengerInterceptor(messengerUser);
 const lInterceptor = new testing.LineInterceptor(lineUser);
+const mAssertion = new testing.MessengerAssertion();
+const lAssertion = new testing.LineAssertion();
 
 beforeEach(() => {
   mInterceptor.use();
@@ -22,20 +25,20 @@ afterEach(() => {
 describe('Hello World', () => {
   // messenger
   it('should reply "Hello World"', async () => {
-    const { server } = testing.setup();
+    const server = bottender.initializeServer();
     
     await testing.sendMessengerTextEvent(server, PSID, process.env.MESSENGER_PAGE_ID, "Hi"); //拆解成兩行
     // 底下結合成Hello World
-    const res = mInterceptor.response();
-    expect(res).toEqual('Hello World!');
+    const body = mInterceptor.parse();
+    mAssertion.textWouldBe(body, "Hello World!");
   });
 
   // line
   it('should reply "Hello World"', async () => {
-    const { server } = testing.setup();
+    const server = bottender.initializeServer();
     
     await testing.sendLineTextEvent(server, userId, "Hi");
-    const res = lInterceptor.response();
-    expect(res).toEqual('Hello World!');
+    const body = lInterceptor.parse();
+    lAssertion.textWouldBe(body, "Hello World!");
   });
 });
